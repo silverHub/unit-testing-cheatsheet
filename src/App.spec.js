31ead083560b80cb25c070f9dd63160f8aa9495b
Tamas Hoffman <tamas_hoffman@epam.com>
2020-10-17 01:25:48 +0200

    first tests run

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,21 +1,19 @@
+import React from 'react';
 import App from "./App";
 import { render, fireEvent, screen } from "@testing-library/react";
-//import "@testing-library/jest-dom/extend-expect";
 
-//describe("example unit tests for hook based components", () => {
 
-/*
+describe("example unit tests for hook based components", () => {
+
+
   beforeEach(() => {
     render(<App />);
   });
-*/
-test("two plus two is four", () => {
-  expect(2 + 2).toBe(4);
-});
 
 test("component rendered first with empty items", () => {
-  render(<App />);
   expect(screen.getByText("Select your choice")).toBeInTheDocument();
+  expect(screen.getByText("Selected:")).toBeInTheDocument();
+  
 });
 
-//});
+});
--- src/useItems.js
+++ src/useItems.js
@@ -10,16 +10,18 @@ const initialItems = [
 const getItems = () => {
   return new Promise((res, rej) => {
     window.setTimeout(() => {
+      //console.log('timeout fn runs')
       res(initialItems);
     }, 500);
-  });
-};
+  })//.then((res)=> { console.log('promise first then', res); return res;})
+}
 
 export default function useItems() {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    getItems().then(setItems);
+    getItems()
+    .then(setItems)
   }, []);
 
   return {
