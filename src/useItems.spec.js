a8a060287cd0b121ba37e63c428408d8b8edd0b5
Tamas Hoffman <tms.hoffman@gmail.com>
2020-10-17 23:49:52 +0200

    sessionTimer example added

---
+++ src/useItems.spec.js
@@ -0,0 +1,38 @@
+import useItems from './useItems';
+import {renderHook, act} from '@testing-library/react-hooks';
+
+describe('custom hook with setTimeout', () => {
+test('init', async () => {
+  const { result, waitForNextUpdate } = renderHook(() => useItems());
+
+  // sessionTimer did not run yet
+  expect(result.current.setItems).toBeDefined();
+  expect(result.current.items).toEqual([]);
+
+  // wait for sessionTimer to expire and trigger react cycle
+  await waitForNextUpdate();
+
+  // setItem triggered, hook returns with proper value
+  expect(result.current.items).toEqual([
+    {
+     "id": "1",
+     "text": "strawberry",
+   },
+    {
+     "id": "2",
+     "text": "pear",
+   },
+    {
+     "id": "3",
+     "text": "apple",
+   },
+    {
+     "id": "4",
+     "text": "grape",
+   },
+ ]);
+});
+
+});
+
+
