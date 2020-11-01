66c85936a8f256a7f04c91de524d19e1891d4c80
Tamas Hoffman <tms.hoffman@gmail.com>
2020-11-01 16:39:38 +0100

    app layout changes

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import ChooserTimeout from "./ChooserTimeout";
 import ChooserFetch from "./ChooserFetch";
 import "./styles.css";
 
-export default function App() {
-  //  const { items, setItems } = useItemsFetch();
+const App = () => (
+  <div className="App">
+    <h1>Cheatsheet</h1>
+    ...
+    <h1>Examples</h1>
+    <h2>Timeout based</h2>
+    <ChooserTimeout />
+    <h2>Server response based</h2>
+    <ChooserFetch />
+  </div>
+);
 
-  return (
-    <div className="App">
-      <h1>Cheatsheet</h1>
-        ...
-      
-      <h1>Examples</h1>
-      <h2>Timeout based</h2>
-      <ChooserTimeout />
-
-      <h2>Server response based</h2>
-      <ChooserFetch />
-
-    </div>
-  );
-}
+export default App;
